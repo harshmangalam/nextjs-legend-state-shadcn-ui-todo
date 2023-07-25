20dc266a1d90c8f110dd14f57fad3e8db3626f72
harshmangalam <harshdev8218@gmail.com>
2023-07-25 22:35:19 +0530

    added card component

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
     <div>
-      <div className="flex flex-col gap-y-4 items-center">
+      <section className="flex flex-col gap-y-4 items-center">
         <h1 className="text-4xl font-bold text-primary">Todo</h1>
         <p className="text-xl text-muted-foreground">
           Built with Next-13 app router | Legend state | Shadcn-ui
         </p>
-      </div>
+      </section>
+
+      <section className="flex w-full max-w-md  mx-auto items-center space-x-2 justify-center mt-8">
+        <Input type="text" placeholder="Start typing..." autoFocus />
+        <Button type="submit">Save</Button>
+      </section>
     </div>
   );
 }
