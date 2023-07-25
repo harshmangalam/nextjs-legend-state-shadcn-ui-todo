98e149cab65ba0812bf241aabc6d203fc0fa1d4c
harshmangalam <harshdev8218@gmail.com>
2023-07-25 22:29:34 +0530

    added Input component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div className="flex flex-col gap-y-4 items-center">
+        <h1 className="text-4xl font-bold text-primary">Todo</h1>
+        <p className="text-xl text-muted-foreground">
+          Built with Next-13 app router | Legend state | Shadcn-ui
+        </p>
+      </div>
+    </div>
+  );
 }
