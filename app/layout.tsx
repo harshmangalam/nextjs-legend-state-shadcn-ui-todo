f85066121e3cc2592c27dfcce623833424c2a30e
harshmangalam <harshdev8218@gmail.com>
2023-07-25 22:42:05 +0530

    added todo component

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,7 +14,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="min-h-screen max-w-md mx-auto bg-muted">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Todo from "@/components/todo";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 
@@ -14,6 +15,12 @@ export default function Home() {
       <section className="flex w-full max-w-md  mx-auto items-center space-x-2 justify-center mt-8">
         <Input type="text" placeholder="Start typing..." autoFocus />
         <Button type="submit">Save</Button>
+      </section>
+
+      <section className="mt-8 flex flex-col gap-y-2">
+        {[...new Array(4)].map((todo) => (
+          <Todo />
+        ))}
       </section>
     </div>
   );
